227dd6e421154f3ba2cb7c9670abfcd547d4ed3f
Wondermutt-v1 <adrian.r.perez@outlook.com>
2024-06-07 19:01:46 -0500

    More updates

--- Marlin/src/gcode/feature/mixing/M163-M165.cpp
+++ Marlin/src/gcode/feature/mixing/M163-M165.cpp
@@ -1,6 +1,6 @@
 /**
  * Marlin 3D Printer Firmware
- * Copyright (c) 2019 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
+ * Copyright (c) 2020 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
  *
  * Based on Sprinter and grbl.
  * Copyright (c) 2011 Camiel Gubbels / Erik van der Zalm
@@ -16,7 +16,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
  */
 
@@ -76,7 +76,7 @@ void GcodeSuite::M164() {
    *   I[factor] Mix factor for extruder stepper 6
    */
   void GcodeSuite::M165() {
-    // Get mixing parameters from the GCode
+    // Get mixing parameters from the G-Code
     // The total "must" be 1.0 (but it will be normalized)
     // If no mix factors are given, the old mix is preserved
     const char mixing_codes[] = { LIST_N(MIXING_STEPPERS, 'A', 'B', 'C', 'D', 'H', 'I') };
